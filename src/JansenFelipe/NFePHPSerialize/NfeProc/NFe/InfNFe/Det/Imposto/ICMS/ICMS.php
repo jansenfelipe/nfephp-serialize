51a41eb660a08824c9b47b9cd9d85c0af10bf1a8
Jansen Felipe <jansen.felipe@gmail.com>
2014-11-20 16:54:26 -0200

    no message

--- src/JansenFelipe/NFePHPSerialize/NfeProc/NFe/InfNFe/Det/Imposto/ICMS/ICMS.php
+++ src/JansenFelipe/NFePHPSerialize/NfeProc/NFe/InfNFe/Det/Imposto/ICMS/ICMS.php
@@ -14,5 +14,11 @@ class ICMS {
      * @Type("JansenFelipe\NFePHPSerialize\NfeProc\NFe\InfNFe\Det\Imposto\ICMS\ICMS60")
      */
     public $ICMS60;
+    
+    
+    /**
+     * @Type("JansenFelipe\NFePHPSerialize\NfeProc\NFe\InfNFe\Det\Imposto\ICMS\ICMSSN102")
+     */
+    public $ICMSSN102;
 
 }
